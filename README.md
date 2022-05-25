1f2db91de20632daa72abd65f41e4fccde7fc6b4
Garry <gurpreet.singh@qualhon.com>
2022-05-25 11:57:49 +0530

    Initial commit
    This is just simple composer package that will print the 'Hello world' message.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World
+
+This is a simple composer packge that just print the "Hello world!"
---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+namespace ninjaProgrammer\helloWorld;
+
+class Index
+{
+    public function greet($greeting = 'Hello world!')
+    {
+        return $greeting;
+    }
+}
